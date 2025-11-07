472ce0677e6f07830145d2d441162295d468845b
Jakub Kobezda <jakub.kobezda@gmail.com>
2025-11-07 16:34:20 +0100

    pridanie testovacieho endpointu

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+DB_SERVER = "localhost"
+DB_USER = "receipts_user"
+DB_PASSWORD = "my_password"
+DB_NAME = "receipts_db"
+PORT = 5432
+
+
 
 @app.get("/")
 async def root():
@@ -11,3 +18,24 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/dball")
+async def get_all_from_db():
+    import psycopg2
+    from psycopg2.extras import RealDictCursor
+
+    conn = psycopg2.connect(
+        host=DB_SERVER,
+        database=DB_NAME,
+        user=DB_USER,   
+        password=DB_PASSWORD,
+        port=PORT
+    )   
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM receipts;")   
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return {"data": records}
+
+    
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,9 @@
+function Landing() {
+  return (
+    <>
+    <h1>Nigga</h1>
+    <p>iuasyhdkjasd</p>
+    </>
+  )
+}
+export default Landing;
